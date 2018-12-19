25e3e6964db0d18047efe39c3aa0d007302dbc55
Prarabdh <prarabdhgrg@gmail.com>
2018-12-20 00:17:54 +0530

    Added backgrond sound

--- app/src/main/java/com/example/prarabdh/task6/FragmentMainQuiz.java
+++ app/src/main/java/com/example/prarabdh/task6/FragmentMainQuiz.java
@@ -1,18 +1,16 @@
 package com.example.prarabdh.task6;
 import android.content.Context;
-import android.net.Uri;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.CountDownTimer;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.google.firebase.database.*;
@@ -20,35 +18,51 @@ import com.google.firebase.database.*;
 import java.util.ArrayList;
 
 
+public class FragmentMainQuiz extends Fragment
+{
 
-public class FragmentMainQuiz extends Fragment {
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
     ProgressBar progressBar;
     TextView question;
     TextView option_1;
     TextView option_2;
     TextView option_3;
     TextView option_4;
+    MediaPlayer mediaPlayer;
+    
     int status=0;
     private Handler handler=new Handler();
-    private OnFragmentInteractionListener mListener;
-    final int NUMBER_OF_QUESTIONS_TOTAL = 3;     //Stores the total number of questions that are stored in the database for the given category
+    final int NUMBER_OF_QUESTIONS_TOTAL = 4;     //Stores the total number of questions that are stored in the database for the given category
     int NUMBER_OF_QUESTIONS_PER_ROUND = 2; //Stores the number of Questions the user will play per round of the quiz
     final String CATEGORY = "Cricket";           //Stores the category user has selected for playing
     int askedQuestionIndices[] = new int[NUMBER_OF_QUESTIONS_TOTAL + 1];//Stores the indices of the questions already asked to the user in this round
     int i = 1;                                   //Stores the number of questions asked in this particular round
-    TextView questionTextView, option1TextView, option2TextView, option3TextView, option4TextView;
     ArrayList<QuestionModel> arrayList = new ArrayList<>();  //Array to store all questions available in that category in the form of QuestionModel objects
     int currentRandom;                     //Stores the index of the currently generated random Question
+    int points,wrong,corect;       //Points variable stores the points of current quiz, wrong variable stores number of incorrect answers, and correct variable stores number of correct answers
 
     //Function to generate a random number
-    public int randomGenerator() {
+    public int randomGenerator()
+    {
         int rand = 0;
         do {
             double x = Math.random() * 100;
+            double y = Math.random() * 100;
+            if(x%2==0 && y%2==0)
+            {
+                x=x+y;
+            }
+            else if(x%2==0 && y%2==1)
+            {
+                x=x*y;
+            }
+            else if(x%2==1 && y%2==0)
+            {
+                x=Math.abs(x-y);
+            }
+            else
+            {
+                x=(x/y)*100;
+            }
             rand = (int) x % NUMBER_OF_QUESTIONS_TOTAL;
             for (int j = 0; j < i; j++) {
                 if (askedQuestionIndices[i] == rand) {
@@ -61,12 +75,33 @@ public class FragmentMainQuiz extends Fragment {
         return rand;
     }
 
+    //This functions resets all properties of textViews to prepare it for the next question
+    public void resetButtons()
+    {
+        option_1.setBackgroundColor(getResources().getColor(R.color.DefaultBackground));
+        option_2.setBackgroundColor(getResources().getColor(R.color.DefaultBackground));
+        option_3.setBackgroundColor(getResources().getColor(R.color.DefaultBackground));
+        option_4.setBackgroundColor(getResources().getColor(R.color.DefaultBackground));
+
+        option_1.setClickable(true);
+        option_2.setClickable(true);
+        option_3.setClickable(true);
+        option_4.setClickable(true);
+
+        question.setText("");
+        option_4.setText("");
+        option_3.setText("");
+        option_2.setText("");
+        option_1.setText("");
+    }
+
 
     @Override
     public void onStart() {
         super.onStart();
         final FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("Categories").child("Cricket").child("Q&A");
+        DatabaseReference myRef = database.getReference("Categories").child(CATEGORY).child("Q&A");
+        mediaPlayer=MediaPlayer.create(getContext(),R.raw.main_quiz_background);
 
         //Gets all the questions stored on the database at that particular instant, and stores them as an array of QuestionModels
         //This is defined outside the for loop, to minimize the read time, and also prevent high data usage
@@ -78,6 +113,7 @@ public class FragmentMainQuiz extends Fragment {
                     QuestionModel questionModel = new QuestionModel(contact.child("Ques").getValue().toString(), contact.child("Option1").getValue().toString(), contact.child("Option2").getValue().toString(), contact.child("Option3").getValue().toString(), contact.child("Option4").getValue().toString(), contact.child("Answer").getValue().toString());
                     arrayList.add(questionModel);
                 }
+                mediaPlayer.start();
                 newQuestion();
             }
 
@@ -87,10 +123,10 @@ public class FragmentMainQuiz extends Fragment {
             }
         });
 
-            option1TextView.setOnClickListener(new onClick(option1TextView));
-            option2TextView.setOnClickListener(new onClick(option2TextView));
-            option3TextView.setOnClickListener(new onClick(option3TextView));
-            option4TextView.setOnClickListener(new onClick(option4TextView));
+            option_1.setOnClickListener(new onClickButton(option_1));
+            option_2.setOnClickListener(new onClickButton(option_2));
+            option_3.setOnClickListener(new onClickButton(option_3));
+            option_4.setOnClickListener(new onClickButton(option_4));
 
 
 
@@ -101,35 +137,39 @@ public class FragmentMainQuiz extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState)
+    {
+        // Inflate the layout for this fragment
         View view=inflater.inflate(R.layout.fragment_fragment_main_quiz, container, false);
-        progressBar=view.findViewById(R.id.ProgressBarMainQuiz);
-        question=view.findViewById(R.id.TextViewQuestion);
-        option_1=view.findViewById(R.id.Option1);
-        option_2=view.findViewById(R.id.Option2);
-        option_3=view.findViewById(R.id.Option3);
-        option_4=view.findViewById(R.id.Option4);
-        new Thread(new Runnable() {
+
+        new Thread(new Runnable()
+        {
             @Override
-            public void run() {
-                while (status<100){
+            public void run()
+            {
+                while (status<100)
+                {
                     status += 1;
                     // Update the progress bar
-                    handler.post(new Runnable() {
-                        public void run() {
+                    handler.post(new Runnable()
+                    {
+                        public void run()
+                        {
                             progressBar.setProgress(status);
                         }
         });
-                    try {
+                    try
+                    {
                         // Sleep for 300 milliseconds.
                         Thread.sleep(300);
-                    } catch (InterruptedException e) {
+                    } catch (InterruptedException e)
+                    {
                         e.printStackTrace();
                     }
                 }
             }
         }).start();
-        // Inflate the layout for this fragment
+
         return view;
     }
 
@@ -137,11 +177,12 @@ public class FragmentMainQuiz extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
     {
         //Initializing all the Views so that they need not be initialized again and again in the code, hence making the code concise
-        questionTextView = view.findViewById(R.id.TextViewQuestion);
-        option1TextView = view.findViewById(R.id.Option1);
-        option2TextView = view.findViewById(R.id.Option2);
-        option3TextView = view.findViewById(R.id.Option3);
-        option4TextView = view.findViewById(R.id.Option4);
+        question = view.findViewById(R.id.TextViewQuestion);
+        progressBar = view.findViewById(R.id.ProgressBarMainQuiz);
+        option_1 = view.findViewById(R.id.Option1);
+        option_2= view.findViewById(R.id.Option2);
+        option_3 = view.findViewById(R.id.Option3);
+        option_4 = view.findViewById(R.id.Option4);
         super.onViewCreated(view, savedInstanceState);
     }
 
@@ -150,22 +191,22 @@ public class FragmentMainQuiz extends Fragment {
     {
         currentRandom = randomGenerator();
         askedQuestionIndices[i - 1] = currentRandom;
-        questionTextView.setText(arrayList.get(currentRandom).getQuestion());
-        option1TextView.setText(arrayList.get(currentRandom).getOption1());
-        option2TextView.setText(arrayList.get(currentRandom).getOption2());
-        option3TextView.setText(arrayList.get(currentRandom).getOption3());
-        option4TextView.setText(arrayList.get(currentRandom).getOption4());
+        question.setText(arrayList.get(currentRandom).getQuestion());
+        option_1.setText(arrayList.get(currentRandom).getOption1());
+        option_2.setText(arrayList.get(currentRandom).getOption2());
+        option_3.setText(arrayList.get(currentRandom).getOption3());
+        option_4.setText(arrayList.get(currentRandom).getOption4());
     }
 
 
     /**Creating a class for onClickListener as the same logic is to be implemented with all the 4 option buttons
      * Didnot crate a new object as I needed a parameter regarding which option was clicked, which was not possible in an object*/
-    public class onClick implements View.OnClickListener
+    public class onClickButton implements View.OnClickListener
     {
 
         TextView textView;  //Stores which textView called this class
 
-        public onClick(TextView t)
+        public onClickButton(TextView t)
         {
             textView = t;
         }
@@ -177,30 +218,43 @@ public class FragmentMainQuiz extends Fragment {
                                                                     //Introduced a new variable instead of using the expression at the right again and again to save the time required by the computer to read the arrayList
                                                                     //and execute the getAnswer() function multiple number of times
 
+            //Disable other textViews so that multiple answers cannot be selected
+            option_1.setClickable(false);
+            option_2.setClickable(false);
+            option_3.setClickable(false);
+            option_4.setClickable(false);
+
+            //Stopping the background sound, and resetting it for the next question
+            mediaPlayer.pause();
+            mediaPlayer.seekTo(0);
+
             if (textView.getText().toString().equals(correct))
             {
                 //Setting background colour to green if the answer is correct
-                textView.setBackgroundColor(getResources().getColor(R.color.CorrectAnswer));
+                textView.setBackgroundColor(0xFF00FF00);
+                points+=1;
+                correct+=1;
             }
             else
             {
-                textView.setBackgroundColor(getResources().getColor(R.color.WrongAnswer));
+                textView.setBackgroundColor(0xFFFF0000);
+                wrong+=1;
                 //Setting green background colour to the textView with the right answer
-                if(option1TextView.getText().toString().equals(correct))
+                if(option_1.getText().toString().equals(correct))
                 {
-                    option1TextView.setBackgroundColor(getResources().getColor(R.color.CorrectAnswer));
+                    option_1.setBackgroundColor(0xFF00FF00);
                 }
-                else if(option2TextView.getText().toString().equals(correct))
+                else if(option_2.getText().toString().equals(correct))
                 {
-                    option2TextView.setBackgroundColor(getResources().getColor(R.color.CorrectAnswer));
+                    option_2.setBackgroundColor(0xFF00FF00);
                 }
-                else if(option3TextView.getText().toString().equals(correct))
+                else if(option_3.getText().toString().equals(correct))
                 {
-                    option3TextView.setBackgroundColor(getResources().getColor(R.color.CorrectAnswer));
+                    option_3.setBackgroundColor(0xFF00FF00);
                 }
                 else
                 {
-                    option4TextView.setBackgroundColor(getResources().getColor(R.color.CorrectAnswer));
+                    option_4.setBackgroundColor(0xFF00FF00);
                 }
             }
 
@@ -210,14 +264,17 @@ public class FragmentMainQuiz extends Fragment {
                 public void onFinish()
                 {
                     //Resetting every textView before changing the question
-                    option1TextView.setBackgroundColor(getResources().getColor(R.color.DefaultBackground));
-                    option2TextView.setBackgroundColor(getResources().getColor(R.color.DefaultBackground));
-                    option3TextView.setBackgroundColor(getResources().getColor(R.color.DefaultBackground));
-                    option4TextView.setBackgroundColor(getResources().getColor(R.color.DefaultBackground));
+                    resetButtons();
+
                     if(i<NUMBER_OF_QUESTIONS_PER_ROUND)
                     {
+                        mediaPlayer.start();
                         newQuestion();
                         i++;
+                    }
+                    else
+                    {
+                        Toast.makeText(getContext(),"Game Over\nScore"+points ,Toast.LENGTH_LONG ).show();
                     }
                 }
 
@@ -234,5 +291,21 @@ public class FragmentMainQuiz extends Fragment {
     public void onAttach(final Context context)
     {
         super.onAttach(context);
+    }
+
+
+    @Override
+    public void onDestroy()
+    {
+        //mediaPlayer.release();
+        super.onDestroy();
+    }
+
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        mediaPlayer.release();
     }
 }
