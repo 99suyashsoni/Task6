2a2b94967688340b4e5c21fd6b9dcf895dae287a
99suyashsoni <45849297+99suyashsoni@users.noreply.github.com>
2018-12-20 20:05:48 +0530

    -

--- app/src/main/java/com/example/prarabdh/task6/HomeActivity.kt
+++ app/src/main/java/com/example/prarabdh/task6/HomeActivity.kt
@@ -2,11 +2,7 @@ package com.example.prarabdh.task6
 
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
-import android.support.v4.app.FragmentManager
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_home_screen.*
-import android.R.attr.fragment
-import android.net.wifi.hotspot2.pps.HomeSp
 import android.support.v4.app.Fragment
 import com.example.prarabdh.task6.R.id.navigation_home
 
@@ -19,7 +15,7 @@ class HomeActivity : AppCompatActivity()
         var selectedFragment: Fragment
         when (item.itemId) {
             navigation_home -> {
-                selectedFragment = Countdown()
+                selectedFragment = HomeFragment()
                 supportFragmentManager.beginTransaction().replace(R.id.homeFragment, selectedFragment).commit()
                 return@OnNavigationItemSelectedListener true
             }
@@ -60,7 +56,7 @@ class HomeActivity : AppCompatActivity()
         bottomNav.setOnNavigationItemSelectedListener(navListener)
 
         if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction().replace(R.id.homeFragment, Countdown()).commit()
+            supportFragmentManager.beginTransaction().replace(R.id.homeFragment, HomeFragment()).commit()
             bottomNav.selectedItemId = navigation_home
         }
 
