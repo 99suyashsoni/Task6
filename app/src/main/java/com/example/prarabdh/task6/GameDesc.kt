b114db601a80870e89f00117c6a7cbfcb1742407
99suyashsoni <45849297+99suyashsoni@users.noreply.github.com>
2018-12-22 00:10:16 +0530

    game decs continue

--- app/src/main/java/com/example/prarabdh/task6/GameDesc.kt
+++ app/src/main/java/com/example/prarabdh/task6/GameDesc.kt
@@ -5,14 +5,36 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 
 class GameDesc: Fragment(){
+
+    private var singlePlayer: Button? = null
+    private var multiPLayer: Button? = null
+    private var gameName: TextView? = null
+    private var gameDescrip: TextView? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.game_desc,container,false)
 
+        singlePlayer = HomeActivity().findViewById(R.id.button2)
+        multiPLayer = HomeActivity().findViewById(R.id.button3)
+        gameName = HomeActivity().findViewById(R.id.textView8)
+        gameDescrip = HomeActivity().findViewById(R.id.textView9)
+
+        gameName!!.text = ""
+        gameDescrip!!.text = ""
+        view.setBackgroundResource(0)
+
+        singlePlayer!!.setOnClickListener {
+
+        }
+
+        multiPLayer!!.setOnClickListener {
+
+        }
+
         return view
     }
-
-
 }
--- app/src/main/java/com/example/prarabdh/task6/MyAdapter.kt
+++ app/src/main/java/com/example/prarabdh/task6/MyAdapter.kt
@@ -10,6 +10,8 @@ import android.widget.Button
 
 class MyAdapter(private val images: IntArray, private val names: Array<String>, private val pointsToUnlock: IntArray, private val playerPoints: Int): RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
 
+    //private val gameDesc = GameDesc()
+
     inner class MyViewHolder(val view:View) : RecyclerView.ViewHolder(view){
 
         internal var button: Button = view.findViewById(R.id.button)
@@ -34,6 +36,7 @@ class MyAdapter(private val images: IntArray, private val names: Array<String>, 
 
             Log.i("Button Check","Button Click Works")
             //HomeActivity().supportFragmentManager.beginTransaction().replace(R.id.homeFragment, GameDesc()).addToBackStack(null).commit()
+
         }
     }
 }
