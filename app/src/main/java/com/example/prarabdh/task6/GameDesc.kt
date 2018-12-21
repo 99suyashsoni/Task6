a20463473a83c8058caf37fba0a1f242b433fa59
99suyashsoni <45849297+99suyashsoni@users.noreply.github.com>
2018-12-22 00:35:29 +0530

    --

--- app/src/main/java/com/example/prarabdh/task6/GameDesc.kt
+++ app/src/main/java/com/example/prarabdh/task6/GameDesc.kt
@@ -13,19 +13,21 @@ class GameDesc: Fragment(){
     private var singlePlayer: Button? = null
     private var multiPLayer: Button? = null
     private var gameName: TextView? = null
-    private var gameDescrip: TextView? = null
+    private var gameDescription: TextView? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.game_desc,container,false)
 
-        singlePlayer = HomeActivity().findViewById(R.id.button2)
-        multiPLayer = HomeActivity().findViewById(R.id.button3)
-        gameName = HomeActivity().findViewById(R.id.textView8)
-        gameDescrip = HomeActivity().findViewById(R.id.textView9)
+        singlePlayer = view.findViewById(R.id.button2)
+        multiPLayer = view.findViewById(R.id.button3)
+        gameName = view.findViewById(R.id.textView8)
+        gameDescription = view.findViewById(R.id.textView9)
 
-        gameName!!.text = ""
-        gameDescrip!!.text = ""
-        view.setBackgroundResource(0)
+        gameName!!.text = "Game 1"
+        gameDescription!!.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore " +
+                             "magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+
+        view.setBackgroundResource(R.drawable.blue_wall)
 
         singlePlayer!!.setOnClickListener {
 
