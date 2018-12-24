035851317aea9784a0b8928b87724462832ab4d1
ishita Aggarwal <e.agrawal03@gmail.com>
2018-12-24 12:07:55 +0530

    Commit

--- app/src/main/java/com/example/prarabdh/task6/ScoreFragment.java
+++ app/src/main/java/com/example/prarabdh/task6/ScoreFragment.java
@@ -17,6 +17,12 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 
 public class ScoreFragment extends Fragment {
@@ -26,7 +32,10 @@ public class ScoreFragment extends Fragment {
     Button replay;
     RecyclerView recyclerView;
     ImageView avtar;
-    final String CATEGORY = "Cricket";
+    public ArrayList<String> acateg=new ArrayList<>();
+    public ArrayList<String> nUnlockPoints=new ArrayList<>();
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
      public ScoreFragment(){
          //required empty public constructor
      }
@@ -51,6 +60,27 @@ public class ScoreFragment extends Fragment {
         avtar=view.findViewById(R.id.imageView4);
         super.onViewCreated(view, savedInstanceState);
     }
+    public void onStart() {
+
+        super.onStart();
+        firebaseDatabase=FirebaseDatabase.getInstance();
+        databaseReference= firebaseDatabase.getReference("Categories");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Iterable<DataSnapshot> allCategory=dataSnapshot.getChildren();
+                for(DataSnapshot Category:allCategory ){
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
     public void initiate(){
          AchievementsAdapter adapter=new AchievementsAdapter(getContext(),ncategories,nImages);
         recyclerView.setAdapter(adapter);
