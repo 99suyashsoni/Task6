56aca2f7c5b2e49f53e05f62aa055a4efef3cf0b
codeHacker321 <akshat1499@gmail.com>
2019-02-07 19:16:39 +0530

    implemented final 1

---
+++ app/src/main/java/com/example/prarabdh/task6/dataModels/QuizData.java
@@ -0,0 +1,17 @@
+package com.example.prarabdh.task6.dataModels;
+
+import com.example.prarabdh.task6.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class QuizData {
+
+    public static int background = R.drawable.blue_wall;
+    public static String heading = "";
+    public static String description = "";
+    public static String[] names = new String[40];
+    public static ArrayList<AchivementDataModel> udrAchievements = new ArrayList<>();
+
+}
